02db87136f3dab05b9926d04d9d4ce1f550b175f
Mahanvali <139391502+Mahanvali@users.noreply.github.com>
2024-07-24 21:53:58 +0330

    Full kelvin support :+1:

--- src/Main.java
+++ src/Main.java
@@ -9,40 +9,66 @@ public class Main {
         System.out.print("Celsius: ");
         double celsiusInput = inputObj.nextDouble();
         double outputFormula = (celsiusInput * 9/5) + 32;
+        System.out.println("///////////////////////////////////////////////////");
         System.out.println(celsiusInput +  "°C" + " -> " + outputFormula + "°F");
+        System.out.println("///////////////////////////////////////////////////");
     }
+
     static void FahrenheitToCelsius(Scanner inputObj){
         System.out.print("Fahrenheit: ");
-        double farenheitInput = inputObj.nextDouble();
-        double outputFormula = (farenheitInput - 32) * 5/9;
-        System.out.println(farenheitInput +  "°F" + " -> " + outputFormula + "°C");
+        double fahrenheitInput = inputObj.nextDouble();
+        double outputFormula = (fahrenheitInput - 32) * 5/9;
+        System.out.println("///////////////////////////////////////////////////");
+        System.out.println(fahrenheitInput +  "°F" + " -> " + outputFormula + "°C");
+        System.out.println("///////////////////////////////////////////////////");
     }
 
     static void FahrenheitToKelvin(Scanner inputObj){
         System.out.print("Fahrenheit: ");
-        double farenheitInput = inputObj.nextDouble();
-        double outputFormula = (farenheitInput - 32) * 5 / 9 + 273.15;
-        System.out.println(farenheitInput +  "°F" + " -> " + outputFormula + "K");
+        double fahrenheitInput = inputObj.nextDouble();
+        double outputFormula = (fahrenheitInput - 32) * 5 / 9 + 273.15;
+        System.out.println("///////////////////////////////////////////////////");
+        System.out.println(fahrenheitInput +  "°F" + " -> " + outputFormula + "K");
+        System.out.println("///////////////////////////////////////////////////");
     }
 
     static void CelsiusToKelvin(Scanner inputObj){
         System.out.print("Celsius: ");
-        double celsiuInput = inputObj.nextDouble();
-        double outputFormula = celsiuInput + 273.15;
-        System.out.println(celsiuInput +  "°C" + " -> " + outputFormula + "K");
+        double celsiusInput = inputObj.nextDouble();
+        double outputFormula = celsiusInput + 273.15;
+        System.out.println("///////////////////////////////////////////////////");
+        System.out.println(celsiusInput +  "°C" + " -> " + outputFormula + "K");
+        System.out.println("///////////////////////////////////////////////////");
     }
 
-    static void KelvinToFahrenheit(Scanner inputObScanner){
-
+    static void KelvinToFahrenheit(Scanner inputObj){
+        System.out.print("Kelvin: ");
+        double kelvinInput = inputObj.nextDouble();
+        double outputFormula = (kelvinInput - 273.15) * 9/5 + 32;
+        System.out.println("///////////////////////////////////////////////////");
+        System.out.println(kelvinInput +  "K" + " -> " + outputFormula + "°F");
+        System.out.println("///////////////////////////////////////////////////");
     }
 
-    static void KelvinToCelsius(Scanner inputObScanner){
-
+    static void KelvinToCelsius(Scanner inputObj){
+        System.out.print("Kelvin: ");
+        double kelvinInput = inputObj.nextDouble();
+        double outputFormula = kelvinInput - 273.15;
+        System.out.println("///////////////////////////////////////////////////");
+        System.out.println(kelvinInput +  "K" + " -> " + outputFormula + "°C");
+        System.out.println("///////////////////////////////////////////////////");
     }
+
     public static void main(String args[]){
         Scanner scannerObj = new Scanner(System.in);    //  Create scanner object to get input
         while (true) {
-            System.out.print("Conversion (CtoF, FtoC, FtoK, CtoK or exit): ");
+            System.out.println("CtoF = Celsius to Fahrenheit");
+            System.out.println("CtoK = Celsius to Kelvin");
+            System.out.println("FtoC = Fahrenheit to Celsius");
+            System.out.println("FtoK = Fahrenheit to Kelvin");
+            System.out.println("KtoF = Kelvin to Fahrenheit");
+            System.out.println("KtoC = Kelvin to Celsius");
+            System.out.print("Conversion (CtoF, CtoK, FtoC, FtoK, KtoF, KtoC or exit): ");
             String conversionString = scannerObj.nextLine();    //  Input for selecting the different conversions
             switch (conversionString.toLowerCase()) {
                 case "ctof":
@@ -58,10 +84,22 @@ public class Main {
                 case "ftok":
                     FahrenheitToKelvin(scannerObj);
                     scannerObj.nextLine();  //  Catch the whitespace so that conversionString doesnt show twice
+                break;
 
                 case "ctok":
                     CelsiusToKelvin(scannerObj);
                     scannerObj.nextLine();  //  Catch the whitespace so that conversionString doesnt show twice
+                break;
+                
+                case "ktof":
+                    KelvinToFahrenheit(scannerObj);
+                    scannerObj.nextLine();  //  Catch the whitespace so that conversionString doesnt show twice
+                break;
+
+                case "ktoc":
+                    KelvinToCelsius(scannerObj);
+                    scannerObj.nextLine();  //  Catch the whitespace so that conversionString doesnt show twice
+                break;
 
                 case "exit":
                     scannerObj.close();
