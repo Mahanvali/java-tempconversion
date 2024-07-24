747e7cf54cb50c7be397bf5575bf71b81c4b1099
Mahanvali <139391502+Mahanvali@users.noreply.github.com>
2024-07-24 21:34:43 +0330

    Add kelvin

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,40 @@ public class Main {
         System.out.print("Celsius: ");
         double celsiusInput = inputObj.nextDouble();
         double outputFormula = (celsiusInput * 9/5) + 32;
-        System.out.println("Farenheight: " + outputFormula);
+        System.out.println(celsiusInput +  "°C" + " -> " + outputFormula + "°F");
     }
     static void FahrenheitToCelsius(Scanner inputObj){
         System.out.print("Fahrenheit: ");
         double farenheitInput = inputObj.nextDouble();
         double outputFormula = (farenheitInput - 32) * 5/9;
-        System.out.println("Celsius: " + outputFormula);
+        System.out.println(farenheitInput +  "°F" + " -> " + outputFormula + "°C");
+    }
+
+    static void FahrenheitToKelvin(Scanner inputObj){
+        System.out.print("Fahrenheit: ");
+        double farenheitInput = inputObj.nextDouble();
+        double outputFormula = (farenheitInput - 32) * 5 / 9 + 273.15;
+        System.out.println(farenheitInput +  "°F" + " -> " + outputFormula + "K");
+    }
+
+    static void CelsiusToKelvin(Scanner inputObj){
+        System.out.print("Celsius: ");
+        double celsiuInput = inputObj.nextDouble();
+        double outputFormula = celsiuInput + 273.15;
+        System.out.println(celsiuInput +  "°C" + " -> " + outputFormula + "K");
+    }
+
+    static void KelvinToFahrenheit(Scanner inputObScanner){
+
+    }
+
+    static void KelvinToCelsius(Scanner inputObScanner){
+
     }
     public static void main(String args[]){
         Scanner scannerObj = new Scanner(System.in);    //  Create scanner object to get input
         while (true) {
-            System.out.print("Conversion (CtoF, FtoC, or exit): ");
+            System.out.print("Conversion (CtoF, FtoC, FtoK, CtoK or exit): ");
             String conversionString = scannerObj.nextLine();    //  Input for selecting the different conversions
             switch (conversionString.toLowerCase()) {
                 case "ctof":
@@ -32,6 +54,14 @@ public class Main {
                     FahrenheitToCelsius(scannerObj); 
                     scannerObj.nextLine();  //  Catch the whitespace so that conversionString doesnt show twice
                 break;
+
+                case "ftok":
+                    FahrenheitToKelvin(scannerObj);
+                    scannerObj.nextLine();  //  Catch the whitespace so that conversionString doesnt show twice
+
+                case "ctok":
+                    CelsiusToKelvin(scannerObj);
+                    scannerObj.nextLine();  //  Catch the whitespace so that conversionString doesnt show twice
 
                 case "exit":
                     scannerObj.close();
